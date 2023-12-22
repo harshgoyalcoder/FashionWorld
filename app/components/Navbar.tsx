a723f3be12764623a5a4d8832548335163278e40
Harsh Goyal <harshgoyal1415@gmail.com>
2023-12-23 00:52:35 +0530

    authentication fixed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,27 +7,16 @@ import SearchIcon from '@mui/icons-material/Search';
 import { useSelector } from 'react-redux';
 import Link from 'next/link';
 import { signIn, signOut } from 'next-auth/react';
+import { Box, Button, Input } from '@mui/material';
 
 const Container = styled.div`
   height:60px;
   `;
-  // ${mobile({height:'50px'})}
-  const Wrapper=styled.div`
- padding:10px 20px;
- display:flex;
- justify-content:space-between;
- `
-//  ${mobile({padding:'10px 0px',justifyContent:"center"})}
-const Left=styled.div`
- flex: 1;
- display: flex;
- align-items: center;
-`;
+ 
 const Language=styled.span`
   font-size: 14px;
   cursor: pointer;
   `;
-  // ${mobile({display:'none'})}
 const SearchContainer=styled.div`
   border: 0.5px solid lightgray;
   display: flex;
@@ -36,70 +25,37 @@ const SearchContainer=styled.div`
   padding: 5px;
   
   `;
-  // ${mobile({marginLeft:'10px'})}
-const Input=styled.input`
-  border: none;
-  `;
-  // ${mobile({width:'50px'})}
-const Center=styled.div`
-  flex: 1;
-  text-align: center;
 
-  `;
-const Logo=styled.div`
-  font-weight: bold;
-  
-  `;
-  // ${mobile({fontSize:'24px'})}
-  // ${mobile({marginLeft:'20px'})}
-const Right=styled.div`
-  display: flex;
-  flex: 1;
-  align-items: center;
-  justify-content: flex-end;
-  gap: 2rem;
-  , justifyContent:"center"})}
-  `;
-  // ${mobile({flex:"2",gap:'1rem'
-const MenuItem=styled.div`
-  font-size: 14px;
-  cursor: pointer;
-  /* margin-left: 25px; */
-  
-  `;
-  // ${mobile({fontSize:"12px",marginLeft:"10px"})}
 
 export default function Navbar() {;
 
   // const quantity= useSelector(state=>state?.cart?.quantity)
+const handleSignOut=()=>{
+  signOut();
+}
 
   return (
     <Container>
-         <Wrapper>
-            <Left>
+      <Box sx={{marginTop:"1rem",display:"flex",justifyContent:"space-around"}}>
+
               <Language>EN</Language>
               <SearchContainer>
                 <Input placeholder="Search"/>
                 <SearchIcon style={{color:'gray',fontSize:16}}/>
               </SearchContainer>
-            </Left>
-            <Center>
-              <Logo>Krishna</Logo>
-            </Center>
-            <Right>
-              <MenuItem>Register</MenuItem>
-              <button onClick={()=>signIn("google")}>Log In</button>
-              <button onClick={signOut}>Sign Out</button>
+              <h3>Krishna</h3>
+              <Button href='/register'>Register</Button>
+              <Button href='login'>Log In</Button>
+              <Button onClick={handleSignOut}>Sign Out</Button>
               <Link href="/cart">
-              <MenuItem>
+              <div>
               <Badge badgeContent={"12"} color="primary">
                <ShoppingCartOutlinedIcon color="action" />
               </Badge>
-              </MenuItem>
+              </div>
               </Link>
-              
-            </Right>
-         </Wrapper>
+      </Box>
+
     </Container>
   )
 }
